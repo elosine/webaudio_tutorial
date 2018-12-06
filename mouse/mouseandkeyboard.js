3835653eb072fdd87fc09d0267c77021662d864b
elosine <justinwenloyang@gmail.com>
2018-12-06 11:42:23 -0500

    up

--- mouse/mouseandkeyboard.js
+++ mouse/mouseandkeyboard.js
@@ -1,8 +1,12 @@
 var mspad = document.getElementById("mspad");
 
+mspad.addEventListener('mousedown', mousemove);
 
-mspad.onmousemove = function(e) {
+
+function mousemove() {
+  mspad.onmousemove = function(e) {
     var x = e.pageX - this.offsetLeft;
     var y = e.pageY - this.offsetTop;
     console.log(x + ' ' + y);
+  }
 }
